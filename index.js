a238e498ef09e8551435c63f9e0608fa66ba2f64
liba-ny <94902436+liba-ny@users.noreply.github.com>
2023-01-26 00:34:31 +0100

    Delete useless comments

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const imgSrcs = [];
 for (let i = 0; i < 10; i++) {
   imgSrcs.push(pics[i].attribs.src);
 }
-// console.log(imgSrcs);
-// push every element in imgSrcs array to its own file
-
 for (let j = 0; j < imgSrcs.length; j++) {
   client.get(imgSrcs[j], (res) => {
     if (j === 9) {
